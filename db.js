1d6eb5fbc38a2e41ee7cc6b0ca4a0227a7e3edf1
TobiasSendyk <144272436+48592475@users.noreply.github.com>
2025-05-13 14:17:10 -0300

    Node_Modules integrado junto a la conexion con la database en el archivo db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+export const config = {
+    host: "ep-weathered-bird-acuuykrn-pooler.sa-east-1.aws.neon.tech",
+    database: "neondb",
+    user: "neondb_owner", 
+    password: "npg_m8jc4BMlwdby",
+    port: 5432,
+    ssl: true
+}
